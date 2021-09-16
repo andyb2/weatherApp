295345ab1912d95804f6a9d46d95dc1a9935b97c
AMB <a.michael.boyle@gmail.com>
2021-09-16 08:30:44 -0400

    weather js displays the json data... just added some divs to display the information.

--- src/components/weather/Weather.js
+++ src/components/weather/Weather.js
@@ -1,22 +1,28 @@
 import React from "react"
 import './index.css'
+
 const Weather = (props) => {
     const { weatherData } = props
     console.log(weatherData)
     return (
         <div className="weatherBox">
-            <h2>Toronto</h2>
-            {/* {weatherData && ( */}
-            <>
-                {/* <h2>Toronto</h2> */}
-                {/* <h2>{weatherData.name}, {weatherData.sys.country}</h2>
+
+            {weatherData && (
+                <>
+                    <h2>{weatherData.weatherRequest.name}, {weatherData.weatherRequest.sys.country}</h2>
                     <h3>Temperature:</h3>
                     <div> {Math.round(280.9 - 273.15).toFixed(0)}</div>
-                    <div>{weatherData.weather[0].description}</div>
-                    <div>{<img src={`http://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`} className="card-img-top" alt="Weather Icon" />}</div> */}
-            </>
-            {/* )} */}
+                    <div>Sunrise: {`${new Date(weatherData.oneCallWeather.current.sunrise * 1000).toLocaleTimeString()}`}</div>
+                    <div>Sunset: {`${new Date(weatherData.oneCallWeather.current.sunset * 1000).toLocaleTimeString()}`}</div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div>{weatherData.weatherRequest.weather[0].description}</div>
+                    <div>{<img src={`http://openweathermap.org/img/wn/${weatherData.weatherRequest.weather[0].icon}@2x.png`} className="card-img-top" alt="Weather Icon" />}</div>
+                </>
+            )}
         </div>
     )
 }
 export default Weather
+
