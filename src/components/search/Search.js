4d2aa06f44c4fe1044a613b130e8583235d2f21d
AMB <a.michael.boyle@gmail.com>
2021-09-16 08:29:21 -0400

    removed geocode api call.. wanted to use it for showing a list of cities the user was trying to search for... too many api requests were happening

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -5,66 +5,47 @@ import './index.css'
 const Search = () => {
 
     const [searched, setSearched] = useState('');
-    const [geoState, setGeoState] = useState()
-    const [weatherData, setWeatherData] = useState()
+    // const [geoState, setGeoState] = useState()
+    const [weatherData, setWeatherData] = useState();
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-
-        const weatherRequest = await fetch(
-            `https://api.openweathermap.org/data/2.5/weather?q=${searched}&appid=${process.env.REACT_APP_API_KEY}`,
-        ).then(r => r.json())
-        console.log(weatherRequest)
-        // const test = await fetch(
-        //     `https://api.openweathermap.org/data/2.5/onecall?lat=${weatherRequest.coord.lat}&lon=${weatherRequest.coord.lon}&appid=${process.env.REACT_APP_API_KEY}`,
-        // ).then(r => r.json())
-
-        if (weatherRequest.cod !== '404') {
-            setWeatherData(weatherRequest)
+        // Weather request used for getting the lat and lon of the search
+        // console.log(searched.trim().length)
+        if (searched === '') {
+            console.error(`Please enter a city`)
         } else {
-            console.error(weatherRequest.cod, weatherRequest.message)
+            const weatherRequest = await fetch(
+                `https://api.openweathermap.org/data/2.5/weather?q=${searched}&appid=${process.env.REACT_APP_API_KEY}`,
+            ).then(r => r.json());
+            console.log(weatherRequest)
+            // weatherRequest lat and lon is then passed into oneCallWeather to receive additional weather information
+            if (!weatherRequest.message) {
+                const oneCallWeather = await fetch(
+                    `https://api.openweathermap.org/data/2.5/onecall?lat=${weatherRequest.coord.lat}&lon=${weatherRequest.coord.lon}&appid=${process.env.REACT_APP_API_KEY}`,
+                ).then(r => r.json());
+                setWeatherData({ weatherRequest, oneCallWeather })
+            } else {
+                console.error(weatherRequest.cod, weatherRequest.message)
+                // alert(weatherRequest.message)
+            }
         }
     }
     const handleChange = async (event) => {
-        setSearched(event.target.value)
-
-        dynamicSearch(event.target.value);
+        setSearched((event.target.value).trim())
     }
     console.log(searched)
-    const dynamicSearch = async (value) => {
-        // const geoCode = await fetch(
-        //     `http://api.openweathermap.org/geo/1.0/direct?q=${test}&limit=5&appid=${process.env.REACT_APP_API_KEY}`
-        // ).then(r => r.json());
-        // setGeoState(test)
-        if (value !== '') {
-            setGeoState([
-                { city: 'aurora, CA' },
-                { city: 'aurora, GB' },
-                { city: 'aurora, US' },
-                { city: 'aurora, MX' },
-            ])
-        } else {
-            setGeoState()
-        }
-    }
-    console.log(geoState)
-
-    const clickLocation = (e) => {
-        console.log(e.target.textContent)
-        setGeoState()
-    }
-
     return (
 
         <div className="container">
             <section className="test">
                 <form onSubmit={handleSubmit}>
                     <input className='searchBar' type='text' onChange={handleChange} />
-                    <div className={geoState ? 'dropDownMenu' : null}>
+                    {/* <div className={geoState ? 'dropDownMenu' : null}>
                         {geoState && geoState.map(e => <div className='autoFillSearch' key={e.city} onClick={clickLocation}>{e.city}</div>)}
-                    </div>
+                    </div> */}
                 </form>
-                <Weather weatherData={weatherData} />
+                <Weather weatherData={weatherData !== undefined ? weatherData : null} />
             </section>
         </div>
     )
